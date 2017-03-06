5f130a933ee32aeb232e5f30ac6df64dfcb5e7c1
Aditya <adityamedhe3@gmail.com>
2017-03-06 18:02:35 +0000

    Initial comit

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-var express = require('express');
+var express = require('express'),
+    parser = require('rss-parser'),
+    isUrl = require('is-url');
 
 var app = express();
+app.use(express.static('static/'));     //static middleware which will serve React app
 
-app.use(express.static('static/'));
+
+// the API route to get new feeds
+app.get('/feed', function(req, res) {
+    
+    //check whether URL provided is valid
+    if(!req.query || !req.query.url || !isUrl(req.query.url)) {
+        res.writeHead(400, {'content-type': 'application/json'});
+        res.write(JSON.stringify({status: 400, message: "Invalid RSS feed URL"}));
+        res.end();
+        return;
+    }
+    
+    parser.parseURL(req.query.url, function(err, parsed) {
+        if(err) {
+            res.writeHead(500, {'content-type': 'application/json'});
+            res.write(JSON.stringify({status: 500, message: err}));
+            res.end();
+            return;
+        }
+        
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.write(JSON.stringify({status: 200, feed: parsed.feed}));
+        res.end();
+    });
+});
 
 app.listen(8080);
--- static/scripts/app.client.js
+++ static/scripts/app.client.js
@@ -1,14 +1,152 @@
+/* global $ */
 var React = require('react'),
-    ReactDOM = require('react-dom');
+    ReactDOM = require('react-dom'),
+    bs = require('react-bootstrap');
 
-class MyComponent extends React.Component {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {status: 0, feed: null, inputUrl: '', loading:false};
+        
+        this.update = this.update.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        
+        this.messages = {
+            0: 'Enter a RSS feed URL and click Load',
+            200: 'Loaded. Enjoy reading!',
+            400: 'There was an error in the URL you provided',
+            500: 'There was an error on the server. Try again'
+        };
+    }
+    
+    update() {
+        this.setState({loading: true});
+        $.ajax({
+            data: {
+                url: this.state.inputUrl
+            },
+            url: '/feed',
+            dataType: 'json',
+            complete: (data) => {
+                data = JSON.parse(data.responseText);
+                this.setState({feed: data.feed, status: data.status, loading: false});
+                
+            }
+        });
+    }
+    
+    handleChange(e) {
+        this.setState({inputUrl: e.target.value});
+    }
+    
     render() {
         return (
             <div>
-                Hi! If you can see this text, you have set up react-express-seed correctly.
+                <Logo />
+                <bs.Col xs={12} md={3}>
+                    <Toolbar forUpdate = {this.update} forChange = {this.handleChange} message = {this.messages[this.state.status]} />
+                </bs.Col>
+                
+                <bs.Col xs={12} md={9}>
+                    <FeedContainer loading={this.state.loading} status={this.state.status} feedProp={this.state.feed} />
+                </bs.Col>
             </div>
         );
     }
 }
 
-ReactDOM.render(<MyComponent />, document.getElementById('app'));
+class Logo extends React.Component {
+    render() {
+        return (
+            <bs.Row>
+                <bs.Col xs={12}  style={{textAlign:'center'}}>
+                    <bs.Well>
+                        <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/rss-512.png" height="100"/>
+                        <h1>Ignite<b>RSS</b></h1>
+                        <hr />
+                        <h3>Sample application made by <a href="https://www.linkedin.com/in/aditya-medhe-0b103830/" target="_blank">@adityamedhe</a> for Ignite solutions</h3>
+                    </bs.Well>
+                </bs.Col>
+            </bs.Row>
+        );
+    }
+}
+
+class Toolbar extends React.Component {
+    render () {
+        return (
+            <bs.Row>
+                <bs.Col xs={12}>
+                    <bs.Panel bsStyle="primary" header="RSS Feed Link">
+                        <bs.Alert bsStyle="info">
+                            <bs.Glyphicon glyph="info-sign" />
+                            &nbsp;
+                            {this.props.message}
+                        </bs.Alert>
+                        
+                        <input className="form-control" type="text" onChange={this.props.forChange} />
+                        <hr />
+                        <bs.Button bsSize="large" bsStyle="success" onClick={this.props.forUpdate}>
+                            <bs.Glyphicon glyph="ok" />
+                            &nbsp;
+                            Load 
+                        </bs.Button>
+                    </bs.Panel>
+                </bs.Col>
+            </bs.Row>
+        )
+    }
+}
+
+class FeedContainer extends React.Component {
+    render () {
+        return (
+            <bs.Row>
+                <bs.Col xs={12}>
+                    {(this.props.loading)?<bs.ProgressBar active now={100} />: null}
+                    {
+                        (this.props.status == 200)?
+                        (<bs.Panel bsStyle="primary" header={this.props.feedProp.title}>
+                            
+                            <h2>{this.props.feedProp.description}</h2>
+                            <a href={this.props.feedProp.link}>{this.props.feedProp.link}</a>
+                            <hr/>
+                            <div  style={{maxHeight:'400px', overflowY:'auto'}}>
+                            {this.props.feedProp.entries.map(function(e) {
+                                return (<Feed key = {e.guid} content = {e}/>)
+                            })}
+                            </div>
+                        </bs.Panel>):
+                        
+                        (this.props.status == 0)?
+                        (<bs.Panel bsStyle="info" header="Welcome">
+                            Enter a URL to get started
+                        </bs.Panel>):
+                        (<bs.Panel bsStyle="danger" header="Oops">
+                            Something has gone wrong. Cannot display feeds.
+                        </bs.Panel>)
+                    }
+                </bs.Col>
+            </bs.Row>
+        );
+    }
+}
+
+class Feed extends React.Component {
+    render() {
+        return (
+            <bs.Panel bsStyle="info" 
+                header = {<h3>{this.props.content.title}</h3>}>
+                
+                <a href = {this.props.content.link}>{this.props.content.link}</a><br/>
+                <i>{this.props.content.pubDate}</i>
+                <hr />
+                
+                <p>{this.props.content.contentSnippet}</p>
+                
+                
+            </bs.Panel>
+        );
+    }
+}
+ReactDOM.render(<App />, document.getElementById('app'));
